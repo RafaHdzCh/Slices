e58d9d5218c263cc631562838586cc693f1830fe
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2022-09-28 21:28:50 -0500

    Se elimino una linea de consola
    Update en el codigo.

--- Assets/Scripts/KillTrigger.cs
+++ Assets/Scripts/KillTrigger.cs
@@ -8,7 +8,6 @@ public class KillTrigger : MonoBehaviour
 	{
 		if(other.tag == "Player")										//Si la etiqueta del objeto que ingresa a la zona tiene la etiqueta player...
 		{
-			Debug.Log("El jugado ha entrado en una zona de muerte.");	//Entonces se muestra este mensaje en consola
 			PlayerController.sharedInstance.Kill();						//y el jugador muere.
 		}
 	}
